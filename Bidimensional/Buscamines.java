54176a0a2f54b7471e30e6981d75d82de8f88b95
tkotsu <timurkotsubey@gmail.com>
2019-10-23 20:53:12 +0200

    buscamines

--- Bidimensional/Buscamines.java
+++ Bidimensional/Buscamines.java
@@ -16,8 +16,8 @@ public class Buscamines{
                 buscaminesdef[i][j]="▓▓";
             }
         }
-
-        for(int i=0;i<bombes;i++){
+        int timur=0;
+        while(timur<bombes){
             posicioa=r.nextInt(linies);
             posiciob=r.nextInt(linies);
 
@@ -27,22 +27,41 @@ public class Buscamines{
             }
             else {
                 buscamines[posicioa][posiciob]=1000;
+                timur++;
             } 
             System.out.print(posicioa+" "+posiciob);
             System.out.print("\n");
         }
         for(int i=0;i<buscamines.length;i++){
-            System.out.print("\n");
             for(int j=0;j<buscamines[0].length;j++){
-                System.out.print(buscaminesdef[i][j]+"\t");
+               
+            }
+        }
+
+        System.out.print("\n");
+        System.out.print("\t");
+        for (int i=0;i<buscamines[0].length;i++){
+            System.out.print((i)+"\t");
+        }
+        for(int i=0;i<buscamines.length;i++){
+            System.out.print("\n");
+            System.out.print("\n");
+            System.out.print((i)+"\t");
+            for(int j=0;j<buscamines[0].length;j++){
+                System.out.print(buscamines[i][j]+"\t");
             }
         }
         System.out.print("\n");
         System.console().readLine("Prem intro per jugar.");
 
-        int acabat=0;
-        while(acabat<bombes){
-
+        boolean acabat=false;
+        boolean explotat = false;
+        while(!acabat && !explotat){
+            String bomba = System.console().readLine("Quina posicio tries? (0-0) ");
+            String []parts = bomba.split("-");
+            int part1 = Integer.parseInt(parts[0]);
+            int part2 = Integer.parseInt(parts[1]);
+            System.out.print(buscamines[part1][part2]);
         }
     }
 }
